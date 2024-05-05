000adda0b060f4c2c9b8d6a79fb3773d56312a6f
ShobhitAgrawal23 <48242343+ShobhitAgrawal23@users.noreply.github.com>
2024-05-05 21:20:31 +0530

    updated got winner function by using dfs

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,15 @@
 import entity.Board;
 import entity.Player;
+import java.util.ArrayList;
 import java.util.Deque;
-import java.util.List;
 import java.util.Scanner;
 
 public class Game {
 
+    public static final String ROW = "ROW";
+    public static final String COLUMN = "COLUMN";
+    public static final String DIAGONAL = "DIAGONAL";
+    public static final String ANTI_DIAGONAL = "ANTI_DIAGONAL";
     Deque<Player> players;
     Board board;
 
@@ -38,7 +42,7 @@ public class Game {
                 players.addFirst(player);
                 continue;
             }
-            // add the input if the given coordinate is empty
+
             boolean symbolAddedSuccessfully = board.addPlayingSymbol( player.getPlayingSymbol(), x, y );
             if(!symbolAddedSuccessfully){
                 System.out.println("Place is already occupied, please try again");
@@ -48,7 +52,7 @@ public class Game {
                 players.addLast( player );
             }
 
-            if(gotWinner(player, x, y)){
+            if(gotWinnerWith(player, x, y)){
                 gameOver = true;
                 board.printBoard();
                 return player;
@@ -60,88 +64,51 @@ public class Game {
 
     }
 
-    private boolean gotWinner(Player player, int x, int y) {
-        int rowMatchCount = 0;
-        int colMatchCount = 0;
-        int diagMatchCount = 0;
-        int antiDiagMatchCount = 0;
-
-        // checking row right
-        for(int i=y; i < board.getSize(); i++){
-            if(board.getPlayingBoard()[x][i]==null || board.getPlayingBoard()[x][i]!=player.getPlayingSymbol()){
-                break;
+    private boolean gotWinnerWith(Player player, int x, int y) {
+        ArrayList<String> directions = new ArrayList<>();
+        directions.add( ROW );
+        directions.add( COLUMN );
+        directions.add( DIAGONAL );
+        directions.add( ANTI_DIAGONAL );
+        for (String direction : directions) {
+            int[] xCoordinates = new int[0];
+            int[] yCoordinates = new int[0];
+            switch (direction) {
+                case ROW:
+                    xCoordinates= new int[]{ 0, 0 };
+                    yCoordinates= new int[]{ -1, 1 };
+                    break;
+                case COLUMN:
+                    xCoordinates= new int[]{ -1, 1 };
+                    yCoordinates= new int[]{ 0, 0 };
+                    break;
+                case DIAGONAL:
+                    xCoordinates= new int[]{ 1, -1 };
+                    yCoordinates= new int[]{ 1, -1 };
+                    break;
+                case ANTI_DIAGONAL:
+                    xCoordinates= new int[]{ 1, -1 };
+                    yCoordinates= new int[]{ -1, 1 };
+                    break;
             }
-            rowMatchCount++;
+            if (validateDirection( player, x, y, xCoordinates, yCoordinates, new boolean[board.getSize()][board.getSize()])>=board.getWinCount())
+                return true;
         }
-        // checking row left
-        for(int i=y-1; i >= 0; i--){
-            if(board.getPlayingBoard()[x][i]==null || board.getPlayingBoard()[x][i]!=player.getPlayingSymbol()){
-                break;
-            }
-            rowMatchCount++;
-        }
-        if(rowMatchCount>=board.getWinCount()){
-            return true;
-        }
-
-        // checking column down
-        for(int i=x; i < board.getSize(); i++){
-            if(board.getPlayingBoard()[i][y]==null || board.getPlayingBoard()[i][y]!=player.getPlayingSymbol()){
-                break;
-            }
-            colMatchCount++;
-        }
-        // checking column up
-        for(int i=x-1; i >= 0; i--){
-            if(board.getPlayingBoard()[i][y]==null || board.getPlayingBoard()[i][y]!=player.getPlayingSymbol()){
-                break;
-            }
-            colMatchCount++;
-        }
-        if(colMatchCount>=board.getWinCount()){
-            return true;
-        }
-
-        // checking diagonal down
-        for(int i=x, j=y; i < board.getSize() && j < board.getSize(); i++, j++){
-            if(board.getPlayingBoard()[i][j]==null || board.getPlayingBoard()[i][j]!=player.getPlayingSymbol()){
-                break;
-            }
-            diagMatchCount++;
-        }
-        // checking diagonal up
-        for(int i=x-1, j=y-1; i >= 0 && j >= 0; i--, j--){
-            if(board.getPlayingBoard()[i][j]==null || board.getPlayingBoard()[i][j]!=player.getPlayingSymbol()){
-                break;
-            }
-            diagMatchCount++;
-        }
-        if(diagMatchCount>=board.getWinCount()){
-            return true;
-        }
-
-        // checking anti diagonal down
-        for(int i=x, j=y; i < board.getSize() && j >= 0; i++, j--){
-            if(board.getPlayingBoard()[i][j]==null || board.getPlayingBoard()[i][j]!=player.getPlayingSymbol()){
-                break;
-            }
-            antiDiagMatchCount++;
-        }
-        // checking anti diagonal up
-        for(int i=x-1, j=y+1; i >= 0 && j < board.getSize(); i--, j++){
-            if(board.getPlayingBoard()[i][j]==null || board.getPlayingBoard()[i][j]!=player.getPlayingSymbol()){
-                break;
-            }
-            antiDiagMatchCount++;
-        }
-        if(antiDiagMatchCount>=board.getWinCount()){
-            return true;
-        }
-
         return false;
+    }
 
-
-
+    private int validateDirection(Player player, int x, int y, int[] xCoordinates, int[] yCoordinates, boolean[][] visited) {
+        if(x<0 || y<0 || x>=board.getSize() || y>=board.getSize() || visited[x][y] || board.getPlayingBoard()[x][y]==null || board.getPlayingBoard()[x][y]!=player.getPlayingSymbol()){
+            return 0;
+        }
+        visited[x][y] = true;
+        int sum = 0;
+        for(int i=0; i<2; i++){
+            int newX = x + xCoordinates[i];
+            int newY = y + yCoordinates[i];
+            sum = 1+ validateDirection( player, newX, newY, xCoordinates, yCoordinates, visited );
+        }
+        return sum;
     }
 
 }
